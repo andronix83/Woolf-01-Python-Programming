9332056ac74c2021d268e6be23ab5e918fdbe517
Andrii Pererva <andronix83@gmail.com>
2025-10-14 01:11:13 +0300

    Improvement of task #4 for homework #4

--- pycore-hw-04/04-assistant-bot/assistant_bot.py
+++ pycore-hw-04/04-assistant-bot/assistant_bot.py
@@ -1,13 +1,55 @@
+def parse_input(user_input: str) -> tuple[str, str]:
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args: str, contacts: dict[str, str]) -> str:
+    name, phone = args
+    if not contacts.get(name):
+        contacts[name] = phone
+        return "Contact added."
+    else:
+        return f"Contact '{name}' already exists."
+
+def change_contact(args: str, contacts: dict[str, str]) -> str:
+    name, phone = args
+    if contacts.get(name):
+        contacts[name] = phone
+        return f"Contact '{name}' updated."
+    else:
+        return "Contact not found."
+
+def show_phone(args: str, contacts: dict[str, str]) -> str:
+    name, = args
+    phone = contacts[name]
+    return phone
+
+def show_all(contacts: dict[str, str]) -> list[str]:
+    return [f"{name}: {phone}" for name, phone in contacts.items()]
+
+
 def main():
+    contacts = {}
     print("Welcome to the assistant bot!")
     while True:
-        command = input("Enter a command: ").strip().lower()
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
 
         if command in ["close", "exit"]:
             print("Good bye!")
             break
+
         elif command == "hello":
             print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "show":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            for contact in show_all(contacts):
+                print(f" - {contact}")
         else:
             print("Invalid command.")
 
