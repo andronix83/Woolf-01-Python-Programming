00145c0923de0d82d642aaa14b8e9e4dbc8cdf00
Andrii Pererva <andronix83@gmail.com>
2025-10-16 13:40:30 +0300

    Copied initial implementation of Assistant Bot from the previous HW

---
+++ pycore-hw-05/04_assistant_bot.py
@@ -0,0 +1,61 @@
+def parse_input(user_input: str) -> tuple[str, list[str]]:
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args: str, contacts: dict[str, str]) -> str:
+    name, phone = args
+    if not contacts.get(name):
+        contacts[name] = phone
+        return f"Contact '{name}' added."
+    else:
+        return f"ERROR: Contact '{name}' already exists!"
+
+def change_contact(args: str, contacts: dict[str, str]) -> str:
+    name, phone = args
+    if contacts.get(name):
+        contacts[name] = phone
+        return f"Contact '{name}' updated."
+    else:
+        return f"ERROR: Contact '{name}' not found!"
+
+def show_phone(args: str, contacts: dict[str, str]) -> str:
+    name, = args
+    if contacts.get(name):
+        phone = contacts[name]
+        return phone
+    else:
+        return f"ERROR: Contact '{name}' not found!"
+
+def show_all(contacts: dict[str, str]) -> list[str]:
+    return [f"{name}: {phone}" for name, phone in contacts.items()]
+
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "show":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            for i, contact in enumerate(show_all(contacts)):
+                print(f"{i+1}. {contact}")
+        else:
+            print("Invalid command.")
+
+
+if __name__ == "__main__":
+    main()
