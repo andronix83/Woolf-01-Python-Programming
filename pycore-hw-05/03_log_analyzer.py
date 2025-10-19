f65291216a2ab5cf7f8cc540214ae663860a7460
Andrii Pererva <andronix83@gmail.com>
2025-10-20 01:25:14 +0300

    Finished implementation of task #3 for homework #4

--- pycore-hw-05/03_log_analyzer.py
+++ pycore-hw-05/03_log_analyzer.py
@@ -4,7 +4,7 @@ import sys
 
 # Only lines with these log levels will be counted and
 # printed (if allowed), other levels will be ignored and skipped
-SUPPORTED_LOG_LEVELS = ("INFO", "DEBUG", "WARNING", "ERROR")
+SUPPORTED_LOG_LEVELS = ["INFO", "DEBUG", "WARNING", "ERROR"]
 
 def parse_cmd_args() -> tuple[str, list[str]]:
     """
@@ -41,29 +41,51 @@ def parse_cmd_args() -> tuple[str, list[str]]:
 
     return args.filename, args.levels
 
-def pretty_print_counters(level_counter):
-    # TODO: Implement output in the table
-    # Skip the items with zero occurrences
-    print(level_counter)
+def validate_log_levels(levels_to_print: list[str]) -> list[str]:
+    """
+    Validates user provided log levels against SUPPORTED_LOG_LEVELS.
+    Removes unsupported levels if they found
+    """
+    if levels_to_print in SUPPORTED_LOG_LEVELS:
+        return levels_to_print
+    else:
+        unsupported_levels = set(levels_to_print) - set(SUPPORTED_LOG_LEVELS)
+        print(f"Warning: Unsupported log levels provided: {', '.join(unsupported_levels)} \n")
+        return list(set(SUPPORTED_LOG_LEVELS) & set(levels_to_print))
 
+def pretty_print_counters(level_counter):
+    """
+    Prints the counters into a formatted table, sorting them in descending order.
+    """
+    print(f"\n{'Log level':<10} | {'Count':<5}")
+    print("-" * 20)
+    for level, count in sorted(level_counter.items(), key=lambda x: x[1], reverse=True):
+        if count > 0:
+            print(f"{level:<10} | {count:<5}")
 
 def log_analyzer():
     log_file_name, levels_to_print = parse_cmd_args()
+    levels_to_print = validate_log_levels(levels_to_print)
+
     level_counter = {x: 0 for x in SUPPORTED_LOG_LEVELS}
 
     # Iterate through the lines of the file
-    # TODO: Handle missing or incorrect file
-    with open(log_file_name, "r") as log_file:
-        for line in log_file:
-            # TODO: Handle incorrect line format
-            _, _, level, *_ = line.strip().split()
-            level_counter[level.upper()] += 1
-            # TODO: Validate provided levels
-            if level in levels_to_print:
-                print(line.strip())
+    try:
+        with open(log_file_name, "r") as log_file:
+            for line in log_file:
+                stripped_line = line.strip()
+                _, _, level, *_ = stripped_line.upper().split()
+                if level in level_counter:
+                    level_counter[level] += 1
+                    if level in levels_to_print:
+                        print(stripped_line)
+    except FileNotFoundError:
+        print(f"Error: File not found at path: {log_file_name}")
+    except Exception as e:
+        print(f"An unexpected error occurred while reading the file: {e}")
 
-        # Output the counters in a table format
-        pretty_print_counters(level_counter)
+    # Output the counters in a table format
+    pretty_print_counters(level_counter)
 
 
 if __name__ == '__main__':
